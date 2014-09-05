8c0f87858b1dc58d23006cd581cfa74d23096a44
Björn Ricks <bjoern.ricks@intevation.de>
2014-09-05 10:34:52 +0200

    Add a ProjectDetailSerializer and EntryDetailSerializer

--- trex/serializers.py
+++ trex/serializers.py
@@ -7,7 +7,7 @@
 
 from rest_framework.serializers import HyperlinkedModelSerializer
 
-from trex.models.project import Project
+from trex.models.project import Project, Entry
 
 
 class ProjectSerializer(HyperlinkedModelSerializer):
@@ -15,3 +15,17 @@ class ProjectSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = Project
         fields = ("url", "name", "description", "active", "created")
+
+
+class ProjectDetailSerializer(HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Project
+        fields = ("name", "description", "active", "created", "entries")
+
+
+class EntryDetailSerializer(HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Entry
+        fields = ("date", "duration", "description", "state", "user", "created")
