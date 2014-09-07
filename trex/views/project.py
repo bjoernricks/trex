2320dd29d23d03562319cfbb5cdf46e46795d79b
Björn Ricks <bjoern.ricks@intevation.de>
2014-09-07 18:07:15 +0200

    Add view to add Zeiterfassung entries from a plain text submission

--- trex/views/project.py
+++ trex/views/project.py
@@ -5,11 +5,14 @@
 # See LICENSE comming with the source of 'trex' for details.
 #
 
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 
 from trex.models.project import Project, Entry
+from trex.parsers import PlainTextParser
 from trex.serializers import (
     ProjectSerializer, ProjectDetailSerializer, EntryDetailSerializer)
+from trex.utils import Zeiterfassung
 
 
 class ProjectListCreateAPIView(generics.ListCreateAPIView):
@@ -22,6 +25,36 @@ class ProjectDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
 
     queryset = Project.objects.all()
     serializer_class = ProjectDetailSerializer
+
+
+class ProjectZeiterfassungAPIView(generics.CreateAPIView):
+
+    queryset = Project.objects.all()
+    parser_classes = (PlainTextParser,)
+    serializer_class = ProjectDetailSerializer
+
+    def create(self, request, *args, **kwargs):
+        try:
+            proj = self.get_object()
+        except Project.DoesNotExist:
+            errors = self._create_errors("Project does not exist")
+            return Response(errors, status=status.HTTP_400_BAD_REQUEST)
+
+        zeiterfassung = Zeiterfassung(request.DATA)
+        try:
+            proj.create_entries_from_zeiterfassung(zeiterfassung)
+        except Exception, e:
+            errors = self._create_errors(str(e))
+            # TODO review if e could contain info not suited for the user
+            return Response(errors, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = self.get_serializer(proj)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED,
+                        headers=headers)
+
+    def _create_errors(self, msg):
+        return {"non_field_errors": [msg]}
 
 
 class EntryDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
