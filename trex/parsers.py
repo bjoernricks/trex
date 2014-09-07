c7fa4500b22104b34b50bbcacc3b64923d6da294
Björn Ricks <bjoern.ricks@intevation.de>
2014-09-07 18:05:47 +0200

    Add a parser for plain text

---
+++ trex/parsers.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+#
+# (c) 2014 Bjoern Ricks <bjoern.ricks@gmail.com>
+#
+# See LICENSE comming with the source of 'trex' for details.
+#
+
+from io import TextIOWrapper
+
+from rest_framework.parsers import BaseParser
+
+
+class PlainTextParser(BaseParser):
+
+    media_type = "text/plain"
+
+    def parse(self, stream, media_type=None, parser_context=None):
+        print "Running PlainTextParser"
+        charset = self.get_charset(media_type)
+        if charset:
+            stream = TextIOWrapper(stream, encoding=charset)
+
+        return stream
+
+    def get_charset(self, media_type):
+        if not media_type:
+            return None
+
+        charset = None
+        msplit = media_type.split(" ");
+        for m in msplit:
+            m = m.strip()
+            if "charset" in m:
+                csplit = m.split("=")
+                if len(csplit) > 1:
+                    charset = csplit[1]
+                    return charset.strip().lower()
+        return None
