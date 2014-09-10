58e1c79ed691bb751c9e95c850c86de7aa293eec
Björn Ricks <bjoern.ricks@intevation.de>
2014-09-10 11:34:39 +0200

    Add a filter to show the duration in hour:minutes format

--- trex/static/js/filters.js
+++ trex/static/js/filters.js
@@ -3,3 +3,16 @@ angular.module('trexFilters', []).filter('checkmark', function() {
         return input ? '\u2713' : '\u2718';
     };
 });
+
+angular.module('trexFilters', []).filter('duration', function() {
+    return function(input) {
+        var minutes = input / 60;
+        var hours = Math.floor(minutes / 60);
+        var minutes = minutes % 60;
+
+        if (minutes.toString().length < 2) {
+            return "" + hours + ":0" + minutes;
+        }
+        return "" + hours + ":" + minutes;
+    };
+});
