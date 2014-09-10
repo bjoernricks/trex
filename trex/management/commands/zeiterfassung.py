83c682264582a345d4c5ada0a8fd2fd540595d1e
Björn Ricks <bjoern.ricks@intevation.de>
2014-09-10 11:29:19 +0200

    Allow to pass the encoding via a command option

--- trex/management/commands/zeiterfassung.py
+++ trex/management/commands/zeiterfassung.py
@@ -5,6 +5,8 @@
 # See LICENSE comming with the source of 'trex' for details.
 #
 
+from optparse import make_option
+
 from django.core.management.base import BaseCommand, CommandError
 
 from trex.models.project import Project
@@ -16,6 +18,11 @@ class Command(BaseCommand):
     args = "<project> <name>"
     help = "Load entries from a zeiterfassung.txt to a project"
 
+    option_list = BaseCommand.option_list + (
+        make_option("-e", "--encoding", dest="encoding", default="utf-8",
+                    help="Encoding of the input file", type="string"),
+    )
+
     def handle(self, *args, **options):
         if len(args) < 2:
             raise CommandError("You must specifiy a project name and a "
@@ -24,12 +31,14 @@ class Command(BaseCommand):
         name = args[0]
         zeiterfassung_txt = args[1]
 
+        encoding = options.get("encoding")
+
         try:
             project = Project.objects.get(name=name)
         except Project.DoesNotExist:
             raise CommandError("Unknown project name %s" % name)
 
         with open(zeiterfassung_txt, "r") as f:
-            zeiterfassung = Zeiterfassung(f)
+            zeiterfassung = Zeiterfassung(f, encoding)
             created = project.create_entries_from_zeiterfassung(zeiterfassung)
             self.stdout.write(u"Loaded %s entries" % created)
