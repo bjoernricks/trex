5915aba6a46d05369a2558c1a578fbd19a9ac50b
Björn Ricks <bjoern.ricks@intevation.de>
2014-09-11 21:15:41 +0200

    Add copyright headers to js files

--- trex/static/js/filters.js
+++ trex/static/js/filters.js
@@ -1,3 +1,9 @@
+// -*- coding: utf-8 -*-
+//
+// (c) 2014 Bjoern Ricks <bjoern.ricks@gmail.com>
+//
+// See LICENSE comming with the source of 'trex' for details.
+//
 'use strict';
 
 var module = angular.module('trexFilters', []);
--- trex/static/js/services.js
+++ trex/static/js/services.js
@@ -1,3 +1,9 @@
+// -*- coding: utf-8 -*-
+//
+// (c) 2014 Bjoern Ricks <bjoern.ricks@gmail.com>
+//
+// See LICENSE comming with the source of 'trex' for details.
+//
 'use strict';
 
 var trexServices = angular.module('trexServices', ['ngResource']);
