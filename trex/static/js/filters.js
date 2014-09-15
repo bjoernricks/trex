80ff9c7eb09bb0d3c4b8faa0907b6e1dc7b6e88f
Björn Ricks <bjoern.ricks@intevation.de>
2014-09-15 22:17:41 +0200

    Add a sumByKey filter

--- trex/static/js/filters.js
+++ trex/static/js/filters.js
@@ -26,3 +26,18 @@ module.filter('duration', function() {
         return "" + hours + ":" + minutes;
     };
 });
+
+module.filter('sumByKey', function() {
+    return function(data, key) {
+        if (typeof(data) === 'undefined' || typeof(key) === 'undefined') {
+            return 0;
+        }
+
+        var sum = 0;
+        for (var i = data.length - 1; i >= 0; i--) {
+            sum += parseInt(data[i][key]);
+        }
+
+        return sum;
+    };
+});
