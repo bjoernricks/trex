359d32e9993e0ac89ec64d919423a5b06a6387fe
Björn Ricks <bjoern.ricks@intevation.de>
2014-10-15 13:58:03 +0200

    Remove some lint warnings

--- trex/static/js/directives.js
+++ trex/static/js/directives.js
@@ -13,27 +13,27 @@ trexDirectives.directive('ngEnter', function() {
         restrict: 'A',
         link: function(scope, element, attrs) {
             element.bind("keydown", function(event) {
-                if(event.which === 13) {
-                    scope.$apply(function () {
+                if (event.which === 13) {
+                    scope.$apply(function() {
                         scope.$eval(attrs.ngEnter);
                     });
                     event.preventDefault();
                 }
             });
         }
-    }
+    };
 });
 
-trexDirectives.directive('fileUpload', function () {
+trexDirectives.directive('fileUpload', function() {
     return {
         restrict: 'A',
-        link: function (scope, element, attrs) {
-            element.bind('change', function (event) {
+        link: function(scope, element, attrs) {
+            element.bind('change', function(event) {
                 var files = event.target.files;
                  for (var i = 0; i < files.length; i++) {
                      scope.$emit("fileSelected", { file: files[i] });
                  }
             });
         }
-    }
+    };
 });
