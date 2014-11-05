899b74a1ec3a23dba11d6ac34fd23589127a683b
Björn Ricks <bjoern.ricks@intevation.de>
2014-11-05 11:51:03 +0100

    Add a UpdateDataSerializerMixin class
    The new serializer class allows to override the constructing of the returned
    data.

--- trex/serializers.py
+++ trex/serializers.py
@@ -5,11 +5,60 @@
 # See LICENSE comming with the source of 'trex' for details.
 #
 
+import warnings
+
+from django.core.paginator import Page
+
 from rest_framework.serializers import (
     HyperlinkedModelSerializer, HyperlinkedIdentityField,
 )
 
 from trex.models.project import Project, Entry, Tag, ProjectUser
+
+
+class UpdateDataSerializerMixin(object):
+
+    @property
+    def data(self):
+        """
+        Returns the serialized data on the serializer.
+        """
+        if self._data is None:
+
+            data = self.get_data()
+
+            self.update_data(data)
+
+            self._data = data
+
+        return self._data
+
+    def update_data(self, data):
+        """
+        Update and add additional data here
+        """
+
+    def get_data(self):
+        """
+        Serializes an model instance or queryset to primitive data
+        """
+        obj = self.object
+
+        if self.many is not None:
+            many = self.many
+        else:
+            many = hasattr(obj, '__iter__') and not isinstance(
+                obj, (Page, dict))
+            if many:
+                warnings.warn('Implicit list/queryset serialization '
+                              'is deprecated. Use the `many=True` flag '
+                              'when instantiating the serializer.',
+                              DeprecationWarning, stacklevel=2)
+
+        if many:
+            return [self.to_native(item) for item in obj]
+        else:
+            return self.to_native(obj)
 
 
 class ProjectSerializer(HyperlinkedModelSerializer):
